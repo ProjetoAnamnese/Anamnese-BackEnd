093223ef11dd4f744fbff26fd47de8bf5a24325f
ivolimajr <ivo.sl.jr@gmail.com>
2024-03-07 17:48:14 -0300

    Propriedade das entidades criadas - sem relacionamento

---
+++ Anamnese.API/ORM/Entity/PacientModel.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Anamnese.API.ORM.Entity
+{
+    public class PacientModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Profession { get; set; }
+        public string Uf { get; set; }
+        public string Birth { get; set; }
+        public string Gender { get; set; }
+    }
+}
