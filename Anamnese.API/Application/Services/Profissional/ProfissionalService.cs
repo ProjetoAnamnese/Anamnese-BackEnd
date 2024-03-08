88ca51ffb538f32bda939980cb51410d17e178a6
Gabriel Santos <gabrielosantosb@outlook.com>
2024-03-08 16:07:41 -0300

    Definindo services profissional

--- Anamnese.API/Application/Services/Profissional/ProfissionalService.cs
+++ Anamnese.API/Application/Services/Profissional/ProfissionalService.cs
@@ -1,6 +1,68 @@
-﻿namespace Anamnese.API.Application.Services.Profissional
+﻿using Anamnese.API.ORM.Entity;
+using Anamnese.API.ORM.Model.ProfissionalModel;
+using Anamnese.API.ORM.Repository;
+
+namespace Anamnese.API.Application.Services.Profissional
 {
     public class ProfissionalService : IProfissionalService
     {
+        private readonly BaseRepository<ProfissionalModel> _profisionalRepository;
+        
+        public ProfissionalModel CreateUser(CreateProfissionalModel createUserModel)
+        {
+            //Verificar se email já existe
+            if (_profisionalRepository.FindAll(e => e.Email == createUserModel.Email).Any()) 
+            {
+                throw new Exception("E-mail já está em uso.");
+            }
+            var salt = BCrypt.Net.BCrypt.GenerateSalt(10);
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(createUserModel.Password, salt);
+
+
+            ProfissionalModel newUser = new ProfissionalModel
+            {
+                Username = createUserModel.Username,
+                Email = createUserModel.Email,
+                Password = hashedPassword,
+            };
+
+            var createdUser = _profisionalRepository.Add(newUser);
+            _profisionalRepository.SaveChanges();
+            return createdUser;
+        }
+
+        public List<ProfissionalModel> GetDoctorsWithPatients()
+        {
+            //Ainda a definir
+            return new List<ProfissionalModel> { };
+
+        }
+
+        public async Task<ProfissionalModel> GetUserByEmailAsync(string email)
+        {
+            return await _profisionalRepository.FindAsync(u => u.Email == email);
+        }
+
+        public bool IsEmailTaken(string email)
+        {
+            return _profisionalRepository.FindAll(e => e.Email == email).Any();
+        }
+
+        public async Task<bool> ValidateCredentials(string email, string password)
+        {
+            var user = await _profisionalRepository.FindAsync(u => u.Email == email);
+            if (user == null)
+            {
+                bool isPasswordCorrect = VerifyPassword(password, user.Password);
+                return isPasswordCorrect;
+
+            }
+            return false;
+        }
+
+        private bool VerifyPassword(string enteredPassword, string storedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(enteredPassword, storedPassword);
+        }
     }
 }
---
+++ Anamnese.API/ORM/Repository/OrderBy.cs
@@ -0,0 +1,8 @@
+﻿namespace Anamnese.API.ORM.Repository
+{
+    public static class OrderBy
+    {
+        public const string Ascending = "ASC";
+        public const string Descending = "DESC";
+    }
+}
