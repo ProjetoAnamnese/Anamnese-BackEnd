e4a97c62db43353f2aede5404c8a934c8dae2be5
Gabriel Santos <gabrielosantosb@outlook.com>
2024-03-13 20:12:56 -0300

    Removendo reportId do PacientModel

--- Anamnese.API/ORM/Entity/PacientModel.cs
+++ Anamnese.API/ORM/Entity/PacientModel.cs
@@ -15,12 +15,8 @@ namespace Anamnese.API.ORM.Entity
         public string Uf { get; set; }
         public DateOnly Birth { get; set; }
         public string Gender { get; set; }
-
         public int ProfissionalId { get; set; }
-        public ProfissionalModel Profissional { get; set; }
-
-        public int? ReportId { get; set; }
-
+        public ProfissionalModel Profissional { get; set; }        
         public ReportModel Report { get; set; }
     }
 }
