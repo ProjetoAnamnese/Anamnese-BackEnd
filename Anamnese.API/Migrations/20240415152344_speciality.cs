8f9da97c3ffa1e28f4c3f517bfda1cd7d2affe3b
Gabriel Santos <gabrielosantosb@outlook.com>
2024-04-15 12:26:59 -0300

    Adicionando MedicalSpeciality no pacient

---
+++ Anamnese.API/Migrations/20240415152344_speciality.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Anamnese.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class speciality : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MedicalSpecialty",
+                table: "Pacient",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MedicalSpecialty",
+                table: "Pacient");
+        }
+    }
+}
--- Anamnese.API/ORM/Entity/PacientModel.cs
+++ Anamnese.API/ORM/Entity/PacientModel.cs
@@ -18,6 +18,7 @@ namespace Anamnese.API.ORM.Entity
         public string Gender { get; set; }
         public int ProfissionalId { get; set; }
         public ProfissionalModel Profissional { get; set; }
+        public string MedicalSpecialty { get; set; }
 
         //[JsonIgnore]
         public ReportModel Report { get; set; }
