c6a4f6d2a5a3e58538594582dadf52f0680c333e
Gabriel Santos <gabrielosantosb@outlook.com>
2024-04-17 16:21:10 -0300

    speciality opcional

--- Anamnese.API/ORM/Entity/ProfissionalModel.cs
+++ Anamnese.API/ORM/Entity/ProfissionalModel.cs
@@ -9,6 +9,6 @@ namespace Anamnese.API.ORM.Entity
         public string? Username { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
-        public string Specialty { get; set; }
+        public string? Specialty { get; set; }
     }
 }
