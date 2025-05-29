9c229894ce5d2dd43df7a617d75cec3bd15a4ef0
gabriel <gabrielosantosb@gmail.com>
2025-05-28 21:21:25 -0300

    Classes para pagedResult e result

---
+++ Anamnese.API/ORM/Model/Common/Result.cs
@@ -0,0 +1,12 @@
+﻿namespace Anamnese.API.ORM.Model.Common
+{
+    public class Result<T>
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public T? Data { get; set; }
+
+        public static Result<T> Ok(T data) => new() { Success = true, Data = data };
+        public static Result<T> Fail(string message) => new() { Success = false, Message = message };
+    }
+}
